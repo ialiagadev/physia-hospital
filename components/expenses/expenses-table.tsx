2ae982609a036e9e6da40d0e6985fc5a2b113c80
Ivan <ialiagadev@gmail.com>
2025-07-21 15:46:41 +0200

    update gastos page

--- components/expenses/expenses-table.tsx
+++ components/expenses/expenses-table.tsx
@@ -135,15 +135,7 @@ export function ExpensesTable() {
       {/* Filters */}
       <div className="flex gap-4 items-center">
         <div className="flex-1">
-          <div className="relative">
-            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
-            <Input
-              placeholder="Buscar en descripción, proveedor o notas..."
-              value={filters.search || ""}
-              onChange={(e) => handleFilterChange("search", e.target.value)}
-              className="pl-10"
-            />
-          </div>
+          
         </div>
 
         <Button variant="outline" onClick={() => setShowFilters(!showFilters)}>
